a57931dea5dd7389ea630ffa001c5d9afae52214
Mehmet Soykan Mutlu <you@example.com>
2022-11-15 12:00:59 +0300

    ikinci commit

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def profile(motifs):
         col = ''.join([motifs[j][i] for j in range(len(motifs))])
         prof.append([float(col.count(nuc))/float(len(col)) for nuc in 'ACGT'])
     return prof
+def find_most_probable_kmer_in_line(line):
+    score = 0.0
+
+
+def profile_most_probable_kmer(dna, k, prof):
+    newMotifs= []
+
 
 # Using readlines() to read from txt dna file.-> returns an array which has every line as element
 file1 = open('dna.txt', 'r')
@@ -26,20 +33,26 @@ k = 10
 
 count = 0
 # Strips the newline character
-temp_List = []
-print("motif list is :",temp_List)
+motif_List = []
+print("motif list is :",motif_List)
 for line in Lines:
     count += 1
     line = line.upper(); # Mutationları upper yaptık
     line = line.strip(); # /n karakterleri ile boşlukları kırptık
     motifStart=random.randint(0,(500-k)); # k ile değişecek. işlemler girilecek k üzerinden olmalı.
-    temp_List.append(line[motifStart:(motifStart+k)])
+    # select random motifs from lines
+    motif_List.append(line[motifStart:(motifStart+k)])
     print(line)
     # select random motifs from lines
 
-print("motif list is :",temp_List)
+print("motif list is :",motif_List)
 
-profileArr=profile(temp_List)
+#calculate profile from motifs
+profileArr=profile(motif_List)
 print(profileArr);
-# select random motifs from lines
+
+
+
+
+
 
